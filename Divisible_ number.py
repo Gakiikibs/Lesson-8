25291433e986f7327d613a34866e6f85596c076f
Gakiikibs <rachelkibangam@gmail.com>
2025-11-17 19:51:04 +0300

    Lesson 8

---
+++ Divisible_ number.py
@@ -0,0 +1,8 @@
+print("Enter a number Numerater")
+numn = int(input())
+print("Enter a number Denomitor")
+numd = int(input())
+if numn%numd==0:
+    print("The number is divisible")
+else: 
+    print("It is not divisible")    
